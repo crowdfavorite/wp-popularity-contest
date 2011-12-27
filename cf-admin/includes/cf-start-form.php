117d9fa10d85c3b8fb6d4a1b72450331a8a7d96f
Alex King <alex@alexking.org>
2011-12-27 09:57:32 -0700

    removing everything

--- cf-admin/includes/cf-start-form.php
+++
@@ -1,2 +0,0 @@
-	<form id="<?php echo $plugin_prefix; ?>_settings_form" name="<?php echo $plugin_prefix; ?>_settings_form" action="<?php echo admin_url('options-general.php'); ?>" method="post" class="cf-form">
-		<input type="hidden" name="cf_action" value="<?php echo $plugin_prefix; ?>_update_settings" />
